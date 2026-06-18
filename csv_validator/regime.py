42c1b7e8ddae6600dfa439718ac17f4c3df2cb8b
Rafal Mach <mach.rafal@gmail.com>
2026-06-18 10:29:30 +0200

    regime-calcs

--- csv_validator/regime.py
+++ csv_validator/regime.py
@@ -19,3 +19,28 @@ class BOCPDDetector:
         self._kappa = np.array([kappa0])
         self._alpha = np.array([alpha0])
         self._beta = np.array([beta0])
+
+    def update(self, x: float) -> float:
+        # 1. Compute log predictive probability for each current run length
+        log_pred = self._log_predictive(x)
+
+        # 2. Compute log joint: log P(r_{t-1}) + log P(x_t | r)
+        log_joint = self._log_probs + log_pred
+
+        # 3. changepoint prob: sum of all runs * hazard, reset to run length 0
+        log_cp = logsumexp(log_joint) + np.log(self.H)
+
+        # 4. growth probs: each run grows by 1, multiplied by (1-H)
+        log_growth = log_joint + np.log(1 - self.H)
+
+        # 5. new log probs: prepend changepoint prob, append growth probs
+        self._log_probs = np.concatenate([[log_cp], log_growth])
+
+        # 6. Normalise
+        self._log_probs -= logsumexp(self._log_probs)
+
+        # 7. Update sufficient statistics for each run length
+        self._update_params(x)
+
+        # 8. Return changepoint probability
+        return float(np.exp(self._log_probs[0]))
